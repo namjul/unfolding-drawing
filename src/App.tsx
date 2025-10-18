e4c0969174002df428cb156eeacafef6ac8839d6
Samuel Hobl <samuel.hobl@gmail.com>
2025-10-18 11:51:13 +0200

    fix flex-item min-height-auto issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <div class="flex gap-2 h-screen p-2">
+    <div class="flex gap-2 h-screen p-2 *:min-w-0">
       <div class="p-2 basis-1/25 grow max-w-100 bg-sky-50">View Controls</div>
-      <canvas class="p-2 basis-1/5 grow  bg-sky-50"></canvas>
+      <canvas class="p-2 basis-1/5 grow bg-sky-50"></canvas>
       <div class="p-2 basis-1/25 grow max-w-100 bg-sky-50">Transformation Controls</div>
     </div>
   );
